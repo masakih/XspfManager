622a25dddd3733f298993cc597612f1b0db6a8e8
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2009-11-07 09:42:56 +0000

    [New] XspfMListViewControllerとそのNibファイルを追加。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@21 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMListViewController.h
@@ -0,0 +1,18 @@
+//
+//  XspfMListViewController.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 09/11/07.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "XspfMViewController.h"
+
+@interface XspfMListViewController : XspfMViewController
+{
+	IBOutlet NSTableView *tableView;
+}
+
+@end
