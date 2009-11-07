a6b0a41d47079a2e5d8036de68d9e4bb4de92d11
Hori,Masaki <masakih@hm.h555.net>
2009-11-07 09:42:56 +0000

    [New] XspfMListViewControllerとそのNibファイルを追加。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@21 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMListViewController.m
@@ -0,0 +1,14 @@
+//
+//  XspfMListViewController.m
+//  XspfManager
+//
+//  Created by Hori,Masaki on 09/11/07.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import "XspfMListViewController.h"
+
+
+@implementation XspfMListViewController
+
+@end
