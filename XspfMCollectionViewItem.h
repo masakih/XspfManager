36e41e556158f3d9c01f34aa62d83a5394a42543
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2009-11-10 15:35:26 +0000

    [New] CollectionViewItemの色を様々に変化させるために色々変更。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@56 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMCollectionViewItem.h
@@ -0,0 +1,22 @@
+//
+//  XspfMCollectionViewItem.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 09/11/10.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMCollectionViewItem : NSCollectionViewItem
+{
+	IBOutlet NSBox *box;
+	IBOutlet NSImageView *thumbnail;
+	
+	// because [self conllectionView] is already nil at [self dealloc].
+	NSCollectionView *collectionViewHolder;	// not retained.
+}
+
+- (void)coodinateColors;
+@end
