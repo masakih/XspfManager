6540bdbb0fac1a3db1c0611ea20ad1d5c042755a
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2009-11-15 01:20:39 +0000

    [New] エンターで開けるようにNSTableViewをサブクラス化。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@70 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMTableView.h
@@ -0,0 +1,16 @@
+//
+//  XspfMTableView.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 09/11/15.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMTableView : NSTableView
+{
+}
+
+@end
