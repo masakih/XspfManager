09970dceb629e13db07047dc9747914461d9a054
Hori,Masaki <masakih@hm.h555.net>
2009-11-15 01:20:39 +0000

    [New] エンターで開けるようにNSTableViewをサブクラス化。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@70 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMTableView.m
@@ -0,0 +1,34 @@
+//
+//  XspfMTableView.m
+//  XspfManager
+//
+//  Created by Hori,Masaki on 09/11/15.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import "XspfMTableView.h"
+
+
+@implementation XspfMTableView
+- (void)keyDown:(NSEvent *)theEvent
+{
+	if([theEvent isARepeat]) return [super keyDown:theEvent];
+	
+#define kRETURN_KEY	36
+#define kENTER_KEY	52
+	
+	unsigned short code = [theEvent keyCode];
+	//	NSLog(@"code -> %d", code);
+	switch(code) {
+		case kRETURN_KEY:
+		case kENTER_KEY:
+			if([self action]) {
+				[self sendAction:[self action] to:[self target]];
+				return;
+			}
+			break;
+	}
+	
+	[super keyDown:theEvent];
+}
+@end
