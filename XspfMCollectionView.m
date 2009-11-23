989939def05bb90efa8c03300100b6f512a5bb87
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2009-11-23 08:47:22 +0000

    [New] XspfCollectionViewに対するファイルのドラックでの登録を実装。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@110 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

--- XspfMCollectionView.m
+++ XspfMCollectionView.m
@@ -10,6 +10,83 @@
 
 
 @implementation XspfMCollectionView
+
+- (void)awakeFromNib
+{
+	[self registerForDraggedTypes:[NSArray arrayWithObjects:NSFilenamesPboardType, nil]];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+	[super drawRect:rect];
+	
+	if(draggingHilight) {
+		NSRect visible = [self visibleRect];
+		[[NSColor selectedControlColor] set];
+//		NSSetFocusRingStyle(NSFocusRingOnly);
+		NSFrameRectWithWidth(visible, 3);
+	}
+}
+
+#pragma mark#### NSDoragging ####
+- (NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender
+{
+	id pb = [sender draggingPasteboard];
+	id plist = [pb propertyListForType:NSFilenamesPboardType];
+	
+	NSWorkspace *ws = [NSWorkspace sharedWorkspace];
+	NSError *error = nil;
+	for(NSString *filePath in plist) {
+		NSString *type = [ws typeOfFile:filePath error:&error];
+		if(![ws type:type conformsToType:@"com.masakih.xspf"]) {
+			return NSDragOperationNone;
+		}
+	}
+	
+	[[[self enclosingScrollView] contentView] setCopiesOnScroll:NO];
+	draggingHilight = YES;
+	[self displayRect:[self visibleRect]];
+	
+	return NSDragOperationCopy;
+}
+- (NSDragOperation)draggingUpdated:(id <NSDraggingInfo>)sender
+{	
+	return [self draggingEntered:sender];
+}
+- (void)draggingExited:(id <NSDraggingInfo>)sender
+{
+	draggingHilight = NO;
+	[self displayRect:[self visibleRect]];
+}
+- (BOOL)prepareForDragOperation:(id <NSDraggingInfo>)sender
+{
+	return YES;
+}
+- (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
+{
+	id pb = [sender draggingPasteboard];
+	id plist = [pb propertyListForType:NSFilenamesPboardType];
+	
+	[[[self window] windowController] registerFilePaths:plist];
+	
+	return YES;
+}
+- (void)concludeDragOperation:(id <NSDraggingInfo>)sender
+{
+//	NSLog(@"Enter method %@", NSStringFromSelector(_cmd));
+}
+- (void)draggingEnded:(id <NSDraggingInfo>)sender
+{
+	[[[self enclosingScrollView] contentView] setCopiesOnScroll:YES];
+	draggingHilight = NO;
+	[self displayRect:[self visibleRect]];
+}
+- (BOOL)wantsPeriodicDraggingUpdates
+{
+	return NO;
+}
+
+#pragma mark#### NSResponder ####
 - (void)mouseDown:(NSEvent *)theEvent
 {
 	if([theEvent clickCount] != 2) return [super mouseDown:theEvent];
