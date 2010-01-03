dd3a620c9f699870a27bf507c038ded157bc27ab
Hori,Masaki <masakih@hm.h555.net>
2010-01-03 12:44:44 +0000

    [New] スプリットビューを導入。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@193 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMDragControl.h
@@ -0,0 +1,24 @@
+//
+//  XspfMDragControl.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 10/01/03.
+//  Copyright 2010 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMDragControl : NSImageView
+{
+	id delegate;
+}
+
+- (void)setDelegae:(id)newDelegate;
+- (id)delegate;
+
+@end
+
+@interface NSObject (XspfMDragControlDelegate)
+- (void)dragControl:(XspfMDragControl *)control dragDelta:(NSSize)delta;
+@end
---
+++ XspfMSplitView.h
@@ -0,0 +1,16 @@
+//
+//  XspfMSplitView.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 10/01/03.
+//  Copyright 2010 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMSplitView : NSSplitView
+{
+}
+
+@end
