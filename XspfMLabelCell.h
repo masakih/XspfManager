dd9d9f7444044f557d7c4173d0bcae12219bfa4a
Hori,Masaki <masakih@hm.h555.net>
2010-01-03 17:20:20 +0000

    [New] XSSPFMXspfObjectにlabelプロパティを追加。ラベルを一部実装。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@204 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMLabelCell.h
@@ -0,0 +1,17 @@
+//
+//  XspfMLabelCell.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 10/01/04.
+//  Copyright 2010 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMLabelCell : NSActionCell
+{
+	NSGradient *gradient;
+}
+
+@end
