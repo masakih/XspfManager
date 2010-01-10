aa3dd5a862d0fad746cc6c2b5d5945fee446f3fe
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2010-01-10 12:54:46 +0000

    [New] XspfMPopUpButtonを作成、追加。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@234 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

---
+++ XspfMPopUpButton.h
@@ -0,0 +1,17 @@
+//
+//  XspfMPopUpButton.h
+//  XspfManager
+//
+//  Created by Hori,Masaki on 10/01/10.
+//  Copyright 2010 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface XspfMPopUpButton : NSButton
+{
+	IBOutlet NSMenu *_menu;
+}
+
+@end
