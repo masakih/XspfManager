e3f847652d51092119c830d7a4d4155e7313a8e4
masaki <masaki@ae6f0248-dcd5-41b5-8401-a3f1d2e3b736>
2010-01-25 15:01:38 +0000

    [New]  水平ドラッグ用にも使えるようにした。内容を描かないモードを用意した。ドラッグ領域を（一部）設定出来るようにした。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfmanager/XspfManager@279 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

--- XspfMDragControl.m
+++ XspfMDragControl.m
@@ -45,37 +45,69 @@
 	[cell setControlSize:NSSmallControlSize];
 	
 	[self setCell:cell];
+	
+	[self setDrawsBackground:YES];
+	[self setVertical:YES];
+	[self setDragPosition:NSImageAlignRight];
 }
 - (void)resetCursorRects
 {
-	[self addCursorRect:[self draggingRect] cursor:[NSCursor resizeLeftRightCursor]];
+	NSCursor *cursor = _vertical ? [NSCursor resizeLeftRightCursor] : [NSCursor resizeUpDownCursor];
+	[self addCursorRect:[self draggingRect] cursor:cursor];
 }
 - (NSRect)draggingRect
 {
 	const CGFloat draggingWidth = 20;
 	
 	NSRect frame = [self frame];
-	frame.origin.x = frame.size.width - draggingWidth;
-	frame.origin.y = 0;
-	frame.size.width = draggingWidth;
+	
+	if(_position == NSImageAlignRight ) {
+		frame.origin.x = frame.size.width - draggingWidth;
+		frame.origin.y = 0;
+		frame.size.width = draggingWidth;
+	} else if(_position == NSImageAlignCenter) {
+		frame.origin.x = 0;
+		frame.origin.y = 0;
+	}
 	
 	return frame;
 }
 - (void)drawRect:(NSRect)rect
 {
-	[super drawRect:rect];
-	
-	[[NSColor darkGrayColor] set];
+	if(drawsBackground)
+		[super drawRect:rect];
 	
 	NSRect drawRect = [self draggingRect];
-	drawRect.origin = NSMakePoint(drawRect.origin.x + 7, 6);
-	drawRect.size.width = 1;
-	drawRect.size.height = 10;
-	NSRectFill(drawRect);
-	drawRect.origin.x += 3;
-	NSRectFill(drawRect);
-	drawRect.origin.x += 3;
-	NSRectFill(drawRect);
+	
+//	[[NSColor redColor] set];
+//	NSFrameRect(drawRect);
+	
+	if(drawsBackground) {
+		[[NSColor darkGrayColor] set];
+	} else {
+		[[NSColor whiteColor] set];
+	}
+	
+	if(_vertical) {
+		drawRect.origin = NSMakePoint(drawRect.origin.x + 7, 6);
+		drawRect.size.width = 1;
+		drawRect.size.height = 10;
+		NSRectFill(drawRect);
+		drawRect.origin.x += 3;
+		NSRectFill(drawRect);
+		drawRect.origin.x += 3;
+		NSRectFill(drawRect);
+	} else {		
+		drawRect.origin.x = NSMidX(drawRect) - 20 / 2;
+		drawRect.origin.y = 5;//(drawRect.size.height - 7.5) / 2;
+		drawRect.size.width = 20;
+		drawRect.size.height = 0.5;
+		NSRectFill(drawRect);
+		drawRect.origin.y += 3;
+		NSRectFill(drawRect);
+		drawRect.origin.y += 3;
+		NSRectFill(drawRect);
+	}
 }
 
 - (void)mouseDown:(NSEvent *)theEvent
@@ -116,5 +148,23 @@
 - (id)delegate
 {
 	return delegate;
+}
+
+- (void)setDrawsBackground:(BOOL)flag
+{
+	drawsBackground = flag;
+	[self setNeedsDisplay];
+}
+- (void)setVertical:(BOOL)flag
+{
+	_vertical = flag;
+	[self setNeedsDisplay];
+	[self resetCursorRects];
+}
+- (void)setDragPosition:(NSImageAlignment)position
+{
+	_position = position;
+	[self setNeedsDisplay];
+	[self resetCursorRects];
 }
 @end
