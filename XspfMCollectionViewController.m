5fe13d381f4abe1687bdfb8252e4eab6544d2c87
Hori,Masaki <masakih@hm.h555.net>
2010-02-05 11:40:42 +0000

    [Fix] Mac OS X 10.5用に調整。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@306 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

--- XspfMCollectionViewController.m
+++ XspfMCollectionViewController.m
@@ -12,6 +12,11 @@
 #import "XspfMCollectionViewItem.h"
 #import "XspfMXspfObject.h"
 
+@interface NSCollectionView(CocoaPrivatemethods)
+- (void)_getRow:(unsigned int *)fp8 column:(unsigned int *)fp12 forPoint:(struct _NSPoint)fp16;
+- (NSRect)_frameRectForIndexInGrid:(unsigned int)fp8 gridSize:(struct _NSSize)fp12;
+- (NSRange)columnCountRange;
+@end
 
 @implementation XspfMCollectionViewController
 
@@ -69,8 +74,48 @@
 }
 
 // QLPreviewPanel support
+- (NSRect)selectionItemRectForLeopard
+{
+	NSRect collectionFrame = [collectionView frame];
+	NSSize itemSize = [collectionView minItemSize];
+	
+	// get right edge item colum.
+	NSPoint rightEdge = NSMakePoint(collectionFrame.size.width - 1, itemSize.height / 2);
+	NSUInteger col = 0;
+	NSUInteger row = 0;
+	[collectionView _getRow:&row column:&col forPoint:rightEdge];
+	
+	// get selected item's row and column.
+	NSUInteger index = [[self representedObject] selectionIndex];
+	NSUInteger maxCol = col;
+	col = index % maxCol;
+	row = index / maxCol;
+	
+	// caluculate selected item view's image view point.
+	NSPoint itemImagePoint;
+	itemImagePoint.x = itemSize.width / 2 + itemSize.width * col;
+	itemImagePoint.y = itemSize.height * .2 + itemSize.height * row;	// CollectionView is fliped.
+	
+	// get item image view.
+	NSView *thumbnail = [collectionView hitTest:itemImagePoint];
+	NSView *view = [[thumbnail superview] superview];
+	
+	NSRect frame = [thumbnail frame];
+	
+	NSRect convertedRect = [view convertRect:frame toView:collectionView];
+	if(!NSIntersectsRect([collectionView visibleRect], convertedRect)) {
+		return NSZeroRect;
+	}
+	
+	frame = [view convertRectToBase:frame];
+	frame.origin = [[view window] convertBaseToScreen:frame.origin];
+	return frame;
+}
 - (NSRect)selectionItemRect
 {
+	if(![collectionView respondsToSelector:@selector(itemAtIndex:)]) {
+		return [self selectionItemRectForLeopard];
+	}
 	id item = [collectionView itemAtIndex:[[self representedObject] selectionIndex]];
 	NSRect rect = [item thumbnailFrameCoordinateBase];
 	return rect;
