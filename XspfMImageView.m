4fb3869d2e847ae5ac028f982e535a541fa5ebdd
Hori,Masaki <masakih@hm.h555.net>
2010-02-07 05:24:34 +0000

    [Mod] アイコンビューでのサムネイルの表示の仕方を変更。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfManager/XspfManager@320 ae6f0248-dcd5-41b5-8401-a3f1d2e3b736

--- XspfMImageView.m
+++ XspfMImageView.m
@@ -9,7 +9,53 @@
 #import "XspfMImageView.h"
 
 
+@interface NSImageCell (CocoaPrivate)
+- (NSRect)_imageRectForDrawing:(id)fp8 inFrame:(NSRect)fp12 inView:(id)fp28;
+@end
+
 @implementation XspfMImageView
+- (NSRect)imageFrame
+{
+	NSImageCell *cell = [self cell];
+	NSRect cellFrame = [self frame];
+	cellFrame.origin = NSZeroPoint;
+	if(![self isEnabled]) {		
+		cellFrame = NSInsetRect(cellFrame, 5, 5);
+	}
+	
+	NSRect frame = [cell _imageRectForDrawing:[cell image] inFrame:cellFrame inView:self];
+	frame = [self convertRect:frame toView:[self superview]];
+	return frame;
+}
+
+- (void)drawRect:(NSRect)rect
+{
+	NSImageCell *cell = [self cell];
+	
+	NSRect cellFrame = [self frame];
+	cellFrame.origin = NSZeroPoint;
+	
+	
+	if(![self isEnabled]) {		
+		[NSGraphicsContext saveGraphicsState];
+		
+		cellFrame = NSInsetRect(cellFrame, 5, 5);
+		
+		NSShadow *shadow = [[[NSShadow alloc] init] autorelease];
+		[shadow setShadowOffset:NSMakeSize(2.8, -2.8)];
+		[shadow setShadowBlurRadius:5.6];
+		[shadow setShadowColor:[NSColor darkGrayColor]];
+		[shadow set];
+		
+		NSRect imageRect = [cell _imageRectForDrawing:[cell image] inFrame:cellFrame inView:self];
+		[[NSColor whiteColor] set];
+		NSRectFill(imageRect);
+		
+		[NSGraphicsContext restoreGraphicsState];
+	}
+	[cell drawInteriorWithFrame:cellFrame inView:self];
+}
+
 - (void)mouseDown:(NSEvent *)theEvent
 {
 	if([theEvent clickCount] != 2) return [super mouseDown:theEvent];
